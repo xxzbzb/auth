255d8f3af67dca7e2be997308df83e5005dd54da
xing xin <xxzbzb@163.com>
2019-10-25 14:59:51 +0800

    初次提交

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <el-container :gutter="24">
+        <el-header>Header</el-header>
+        <el-main >
+					<mainContainer></mainContainer>
+				</el-main>
+        <el-footer>Footer</el-footer>
+    </el-container>
+</template>
+
+<script>
+import mainContainer from '@/components/MainContainer'
+export default {
+  name: 'app',
+	components: {
+		mainContainer
+	}
+}
+</script>
+
+<style>
+    html,body,#app,.el-container{
+        /*设置内部填充为0，几个布局元素之间没有间距*/
+        padding: 0px;
+        /*外部间距也是如此设置*/
+        margin: 0px;
+        /*统一设置高度为100%*/
+        height: 100%;
+    }
+    .el-header {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        height: 80px !important;
+    }
+
+    .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        height: 26px !important;
+    }
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+        width: ;
+    }
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+    }
+</style>
---
+++ src/views/Foot.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+	export default {
+		name: "Foot"
+	}
+</script>
+
+<style scoped>
+
+</style>
